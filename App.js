3b166f9a0977c1e987624543c1c2e1cc9251cd52
agoodjoe637 <guzelsmt2201@gmail.com>
2025-11-06 22:42:15 +0300

    Setup tab navigation structure

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+
+import HomeScreen from './screens/HomeScreen';
+import ReportsScreen from './screens/ReportsScreen.js';
+
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen 
+          name="Ana Sayfa" 
+          component={HomeScreen} 
+        
+        />
+        <Tab.Screen 
+          name="Raporlar" 
+          component={ReportsScreen} 
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
